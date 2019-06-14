0de4043992556779d2f687de2b481c61f164e1f4
Bevin Curry <c.larsoncurry425@gmail.com>
2019-06-14 04:31:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,11 @@
 function basicTeenager(age) {
-  if (age >= 13 && age <= 19)
-  return ("You are a teenager!")
+  var age = 16;
+  if (age >= 13) { 
+  return ("You are a teenager!");
+} else if (age <= 19) {
+  return ("You are a teenager!");
+} else (){
+  return ("You are not a teenager");
 }
 
 function teenager(age) {
